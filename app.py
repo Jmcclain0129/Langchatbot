67d034ed00568ccf5c37beaed1c4a780f04d85e0
jmcclain0129 <jmcclain0129@gmail.com>
2023-06-18 14:14:23 +0100

    updated os.getenv to [OPENAI_API_KEY]

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
+import sys
+import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-import os
-import streamlit as st
+
+print("Python executable:", sys.executable)
 
 def main():
     # If running on Streamlit Sharing, use the secrets management system
@@ -15,7 +18,8 @@ def main():
     else:
         from dotenv import load_dotenv
         load_dotenv()
-        os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_KEY")
+        os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+
 
 
     st.set_page_config(page_title="Ask your PDF")
